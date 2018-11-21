f0588a1f4b8d065c76d28524f5470ba235c8bfc4
LAPTOP-N579VG28\Nithin Drake <nithindrake@gmail.com>
2018-11-21 12:49:53 +0530

    Created background scroll effect,bird controls
    Created background scroll effect,bird controls

---
+++ Assets/BirdControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdControl : MonoBehaviour {
+
+private Rigidbody2D rb;
+public float flyPower=2f;
+void Start()
+{
+  rb=GetComponent<Rigidbody2D>();
+}
+void Update()
+{
+    // if(Input.GetMouseButtonDown(0))                 //For testing with mouse controls
+    // {
+    //   rb.velocity=new Vector2(rb.velocity.x,flyPower);
+    // }
+
+    if(Input.touchCount>0)
+    {
+      rb.velocity=new Vector2(rb.velocity.x,flyPower);
+    }
+
+}
+	
+}
---
+++ Assets/ScrollEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollEffect : MonoBehaviour {
+
+private Vector2 startPos;
+private float scrollSpeed=-1f;
+
+void Start()
+{
+    startPos=transform.position;
+}
+
+void Update()
+{
+   //loos the first argument within 0 to second argument by modulo division
+   float newPos=Mathf.Repeat(Time.time*scrollSpeed,8);  
+   //the newPos is multiplied to x axis for forard movement
+   transform.position=startPos+Vector2.right*newPos;   
+  // Debug.Log(transform.position);
+}
+	
+}
