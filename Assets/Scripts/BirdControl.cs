c9ed5b8058198c1b5322bd781802719a181e6eed
LAPTOP-N579VG28\Nithin Drake <nithindrake@gmail.com>
2018-11-23 21:28:33 +0530

    Added new animations for bird,removed old scrolling effects
    Added new animations for bird,removed old scrolling effects

---
+++ Assets/Scripts/BirdControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdControl : MonoBehaviour {
+
+private Rigidbody2D rb;
+private Animator anim;
+public float vertVelocity=2f;
+void Start()
+{
+  rb=GetComponent<Rigidbody2D>();
+  anim=GetComponent<Animator>();
+}
+void Update()
+{
+    //For playtesting with mouse 
+    if(Input.GetMouseButtonDown(0))                 
+    {
+      rb.velocity=new Vector2(rb.velocity.x,vertVelocity);
+      anim.SetBool("isFlapping",true);
+      Debug.Log("Flap");
+    }
+    else{
+      anim.SetBool("isFlapping",false);
+    }
+
+    if(Input.touchCount>0)
+    {
+      rb.velocity=new Vector2(rb.velocity.x,vertVelocity);
+    }
+  
+  
+}
+	
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+public BirdControl bird;
+public Text scoreText;
+public Text gameOverText;
+
+void Start()
+{
+    
+}
+}
