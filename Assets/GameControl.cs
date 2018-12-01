2d2dc4a002eef84e4018f753b8b8d5139fce6880
LAPTOP-N579VG28\Nithin Drake <nithindrake@gmail.com>
2018-12-01 19:29:15 +0530

    Implemented singleton pattern
    implemented singleton pattern to make sure only one instance of GameControl is present at a time

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameControl : MonoBehaviour {
 
-//public GameControl instance;
 public Text scoreText;
 public Text gameOverText;
 public int score=0;
+
+private int gameControls=0;
+
+//implemented singleton pattern to make sure only one instance of GameControl is present at a time
+void Awake()
+{
+  gameControls=GameObject.FindObjectsOfType<GameControl>().Length;
+  if(gameControls>1)
+  {
+    Destroy(gameObject);
+  }
+  else
+  {
+      DontDestroyOnLoad(gameObject);
+  }
+}
     
 void Start()
 {
